33fabf1106af182712761028139cc9fec68c6aaa
github-ci <you@example.com>
2025-11-18 21:14:56 +0800

    fix test

---
+++ src/Mock/MockUserLoader.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tourze\WechatOfficialAccountContracts\Mock;
+
+use Tourze\WechatOfficialAccountContracts\OfficialAccountInterface;
+use Tourze\WechatOfficialAccountContracts\UserInterface;
+use Tourze\WechatOfficialAccountContracts\UserLoaderInterface;
+
+class MockUserLoader implements UserLoaderInterface
+{
+    /**
+     * @var array<string, UserInterface>
+     */
+    private array $usersByOpenId = [];
+
+    /**
+     * @var array<string, UserInterface>
+     */
+    private array $usersByUnionId = [];
+
+    public function addUser(UserInterface $user): self
+    {
+        $this->usersByOpenId[$user->getOpenId()] = $user;
+
+        if (null !== $user->getUnionId()) {
+            $this->usersByUnionId[$user->getUnionId()] = $user;
+        }
+
+        return $this;
+    }
+
+    public function loadUserByOpenId(string $openId): ?UserInterface
+    {
+        return $this->usersByOpenId[$openId] ?? null;
+    }
+
+    public function loadUserByUnionId(string $unionId): ?UserInterface
+    {
+        return $this->usersByUnionId[$unionId] ?? null;
+    }
+
+    public function syncUserByOpenId(OfficialAccountInterface $officialAccount, string $openId): ?UserInterface
+    {
+        return $this->loadUserByOpenId($openId);
+    }
+}
--- tests/MockUserLoader.php
+++ tests/MockUserLoader.php
@@ -4,45 +4,11 @@ declare(strict_types=1);
 
 namespace Tourze\WechatOfficialAccountContracts\Tests;
 
-use Tourze\WechatOfficialAccountContracts\OfficialAccountInterface;
-use Tourze\WechatOfficialAccountContracts\UserInterface;
-use Tourze\WechatOfficialAccountContracts\UserLoaderInterface;
+use Tourze\WechatOfficialAccountContracts\Mock\MockUserLoader as BaseMockUserLoader;
 
-class MockUserLoader implements UserLoaderInterface
+/**
+ * @deprecated Use Tourze\WechatOfficialAccountContracts\Mock\MockUserLoader instead
+ */
+class MockUserLoader extends BaseMockUserLoader
 {
-    /**
-     * @var array<string, UserInterface>
-     */
-    private array $usersByOpenId = [];
-
-    /**
-     * @var array<string, UserInterface>
-     */
-    private array $usersByUnionId = [];
-
-    public function addUser(UserInterface $user): self
-    {
-        $this->usersByOpenId[$user->getOpenId()] = $user;
-
-        if (null !== $user->getUnionId()) {
-            $this->usersByUnionId[$user->getUnionId()] = $user;
-        }
-
-        return $this;
-    }
-
-    public function loadUserByOpenId(string $openId): ?UserInterface
-    {
-        return $this->usersByOpenId[$openId] ?? null;
-    }
-
-    public function loadUserByUnionId(string $unionId): ?UserInterface
-    {
-        return $this->usersByUnionId[$unionId] ?? null;
-    }
-
-    public function syncUserByOpenId(OfficialAccountInterface $officialAccount, string $openId): ?UserInterface
-    {
-        return $this->loadUserByOpenId($openId);
-    }
 }
